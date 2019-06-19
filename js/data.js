cafb536c63fe6a1af2e2be6eb3398982dd0e572b
evilming <evilyym@163.com>
2019-06-19 15:11:36 +0800

    签到初始设置值外放到data文件

--- js/data.js
+++ js/data.js
@@ -3,4 +3,14 @@
 var id = setInterval(function() {}, 0);
 while (id--) clearInterval(id); 
 */
-var Check ={title:'hi现场',mun:20};
+var Check = {
+    title: 'hi现场', mun: 4, logoUrl: 'http://res3.hixianchang.com/qn/img/hi-logo.png', footerText: '这里自定义',
+    CheckArr: [
+        { img: "https://res3.hixianchang.com/qn/wx/5bf1e053bf60724905b71ef2eb15cfe7.jpg" ,name:'叮当猫0'},
+        { img: "https://res3.hixianchang.com/qn/wx/5bf1e053bf60724905b71ef2eb15cfe7.jpg" ,name:'叮当猫1'},
+        { img: "https://res3.hixianchang.com/qn/wx/5bf1e053bf60724905b71ef2eb15cfe7.jpg" ,name:'叮当猫2'},
+        { img: "https://res3.hixianchang.com/qn/wx/5bf1e053bf60724905b71ef2eb15cfe7.jpg" ,name:'叮当猫3'}
+    ],
+    footer:'这里是尾部',
+    time:3500
+};
--- 签到/js/Check.js
+++ 签到/js/Check.js
@@ -1,8 +1,22 @@
-function add(params) {
+function add(arr) {
     $(".rotatein").removeClass("rotatein");
-    $('section').prepend(`<div class="pen rotatein">
-    <img src="https://res3.hixianchang.com/qn/wx/5bf1e053bf60724905b71ef2eb15cfe7.jpg" alt="" srcset="">
-    <p>叮当猫</p>
+    $('section .com').prepend(`<div class="pen rotatein">
+    <img src="${arr.img}" alt="" srcset="">
+    <p>${arr.name}</p>
 </div>`)
 }
-setInterval(add, 5000);
+var i=0;
+var setInt = setInterval(function () {
+    if (i<Check.CheckArr.length) {
+        add(Check.CheckArr[i]);
+        i++;
+    }else{
+        clearInterval(setInt);
+        console.info('签到结束')
+    }
+}, Check.time);
+
+$('.logo img').attr('src',Check.logoUrl);
+$('.title p').text(Check.title);
+$('.flex span').text(Check.mun);
+$("footer p").text(Check.footer)
